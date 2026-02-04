b1f0c8fabeaf9c2239e9548452fad92243cf268e
Evan Liem <evan.sl1307@gmail.com>
2026-02-03 19:35:50 -0500

    Add Supabase client configuration

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tznhxoumebhmgfbivhpp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6bmh4b3VtZWJobWdmYml2aHBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjM3MjUsImV4cCI6MjA4NTczOTcyNX0.VPiSX6aXu81YotbU0VnPNY7kwOAXuh6yrrWxkK-kQvk'; 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
